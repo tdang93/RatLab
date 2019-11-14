1a89c3f44e2780ada1a82b083f8d7820f269ceda
Unknown <aznnoodle101@hotmail.com>
2019-11-14 15:09:07 -0800

    Began some basic Maze elements
    * Created Maze.cs
    * Created Maze empty GameObject
    * Created MazeCalculations.txt in Documentations folder

---
+++ Assets/Scripts/Maze.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maze : MonoBehaviour {
+    private GameObject cell;
+    
+    private void Start() {
+        
+    }
+
+    private void GenerateMaze() {
+        for(int i = 0; i < 16; i++) {
+            for(int j = 0; j < 16; j++) {
+                //GameObject.Instantiate(cell, )
+            }
+        }
+    }
+}
