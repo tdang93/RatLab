119d4a4ada1ccb6e2a4f80e3c5747feb3d8ae2a3
Unknown <aznnoodle101@hotmail.com>
2019-11-26 15:12:17 -0800

    Updated Cell class and created CellOrWall parent class
    * Created CellOrWall parent class that Cell and Wall inherit from
       * (WIP) Created north, south, east, west CellOrWall variables inside of Cell to account for adjacent possibilities. If there is no adjacent Cell, then the corresponding direction has a Wall. If there is an adjacent Cell, then that direction is a Cell.

--- Assets/Scripts/Maze.cs
+++ Assets/Scripts/Maze.cs
@@ -3,8 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Maze : MonoBehaviour {
-    [SerializeField] private GameObject cell;
-    
+    [SerializeField] private GameObject cellPrefab;
+    [SerializeField] private GameObject wallPrefab;
+    [SerializeField] private GameObject postPrefab;
+
+    //[SerializeField] private Cell[][] cellArray;
+    ///*
+    public class CellOrWall {
+        public CellOrWall() {
+
+        }
+    }
+    //*/
+
+    private class Cell : CellOrWall {
+        private GameObject cell;
+        /*
+        public CellOrWall north;
+        public CellOrWall south;
+        public CellOrWall east;
+        public CellOrWall west;
+        */
+        public Cell(GameObject prefab, Vector3 position, Transform parent, string name) {
+            cell = Instantiate(prefab);
+            cell.transform.position = position;
+            cell.transform.parent = parent;
+            cell.name = name;
+            cell.SetActive(true);
+        }
+    }
+
+    private class Wall : MonoBehaviour {
+        GameObject wall;
+        /*
+        Post postA;
+        Post postB;
+
+        public Wall (Cell cellA) {
+
+        }
+
+        public Wall (Cell cellA, Cell cellB) {
+
+        }
+        */
+    }
+    /*
+    private class Post : MonoBehaviour {
+        GameObject post;
+    }
+    */
     private void Start() {
         GenerateMaze();
     }
@@ -13,22 +61,30 @@ public class Maze : MonoBehaviour {
         for(int x = 0; x < 16; x++) {
             for(int z = 0; z < 16; z++) {
                 Vector3 position = this.gameObject.transform.position + new Vector3(x, 0, z);
-                GameObject tile = GenerateTile();
+                GameObject tile = GenerateTile(x, z);
                 tile.name = "Tile (" + x + ", " + z + ")";
-                GameObject.Instantiate(tile, position, Quaternion.identity, this.gameObject.transform);
+                tile.transform.SetParent(this.gameObject.transform);
             }
         }
     }
 
-    private GameObject GenerateTile() {
+    private GameObject GenerateTile(int x, int z) {
         GameObject tile = new GameObject();
-        for(int x = 0; x < 3; x++) {
-            for(int z = 0; z < 3; z++) {
-                Vector3 position = this.gameObject.transform.position + new Vector3(x, 0, z);
-                GameObject tempCell = GameObject.Instantiate(cell, position, Quaternion.identity, tile.transform);
-                tempCell.name = "Cell (" + x + ", " + z + ")";
+        for(int a = 0; a < 3; a++) {
+            for(int c = 0; c < 3; c++) {
+                Vector3 position = this.gameObject.transform.position + new Vector3(x + a, 0, z + c);
+                Cell tempCell = new Cell(cellPrefab, position, tile.transform, "Cell (" + a + ", " + c + ")");
+                //Transform T = tile.transform;
+                //Cell tempCell = new Cell(cellPrefab, T.position, T, "");
+                //cellArray[x + a][z + c] = tempCell;
             }
         }
         return tile;
     }
+    /*
+    private GameObject GeneratePosts() { // TODO
+        GameObject post = GameObject.Instantiate(postPrefab);
+        return post;
+    }
+    */
 }
